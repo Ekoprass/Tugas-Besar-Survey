843d3c1b6e2b694de07e4fdfb930c0c8f7772ea3
KDraxler <39791072+KDraxler@users.noreply.github.com>
2018-07-17 10:11:38 +0700

    updateView_17

--- Survey/application/views/survey/editsurvy.php
+++ Survey/application/views/survey/editsurvy.php
@@ -4,26 +4,55 @@
 	<div class="container">
 		<h2>Edit Survey</h2>
 		
-		<?php form_open('survey/editSurvey'); 
+		<?php 
 			foreach ($surveyid as $key) {
+			$str=explode('-', $key['waktu_survey']);
+		    $tgl=$str[2].'-'.$str[1].'-'.$str[0];
+		
+		echo form_open('survey/edit/'.$key['id_survey'].''); 
+
+		if ($key['status']=='Aktif-Published') {
+	
 		?>
 
 			<div class="form-group">
 			  <label for="usr">Nama Survey:</label>
-			  <input  type="text" class="form-control" name="nama_survey" value="<?php echo $key['nama_survey'] ?>">
+			  <input  type="text" class="form-control" name="nama_survey" value="<?php echo $key['nama_survey'] ?>" disabled>
 			</div>
 			<div class="form-group">
 			  <label for="usr">Deskripsi:</label>
-			  <input type="text" class="form-control" name="deskripsi" value="<?php echo $key['deskripsi'] ?>">
+			  <input type="text" class="form-control" name="deskripsi" value="<?php echo $key['deskripsi'] ?>" disabled>
 			</div>
 			<div class="form-group">
 			  <label for="usr">Waktu survey:</label>
-			  <input type="date" class="form-control" name="waktu_survey" value="<?php echo $key['waktu_survey'] ?>">
+			  <input type="date" class="form-control" name="waktu_survey" value="<?php echo $tgl ?>" disabled>
+			</div>
+			
+			
+		<?php 
+			}	else{?>
+
+			<div class="form-group">
+			  <label for="usr">Nama Survey:</label>
+			  <input  type="text" class="form-control" name="nama_survey" value="<?php echo $key['nama_survey'] ?>" >
+			</div>
+			<div class="form-group">
+			  <label for="usr">Deskripsi:</label>
+			  <input type="text" class="form-control" name="deskripsi" value="<?php echo $key['deskripsi'] ?>" >
+			</div>
+			<div class="form-group">
+			  <label for="usr">Waktu survey:</label>
+			  <input type="date" class="form-control" name="waktu_survey" value="<?php echo $tgl ?>" >
 			</div>
 			
 			
 			<button type="submit" class="btn btn-primary">Submit</button>
-		<?php }
+
+			<?php }
+
+
+			}
+
 			form_close();
 		 	echo anchor('survey', 'Back', "class='btn btn-danger'"); ?>
 		
